469037524f4689702b588f0b3c6c7b6247758f76
Ross <rdegnen@gmail.com>
2015-05-29 10:09:50 -0400

    added jshint to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
 'use strict';
 
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
+// run 'gulp jshint' in the console to lint every js file under the server folder
+// and all its sub folders
+gulp.task("jshint", function(){
+  gulp.src("./server/**/*.js")
+    .pipe(jshint())
+    .pipe(jshint.reporter("jshint-stylish"));
+});
