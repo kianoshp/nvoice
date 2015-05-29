af897be1464f25c9cfa5d4e6a688aa78fd32dbf3
Ross <rdegnen@gmail.com>
2015-05-29 11:53:39 -0400

    added gulp-mocha for tests and gulp-util for logging

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var livereload = require('gulp-livereload');
+var mocha = require('gulp-mocha');
+var gutil = require('gulp-util');
 
 // run 'gulp jshint' in the console to run jshint on every .js file under the server folder
 // and all its sub folders
@@ -13,6 +15,13 @@ gulp.task("jshint", function(){
     .pipe(jshint.reporter("jshint-stylish"));
 });
 
+gulp.task('test', function(){
+  return gulp.src(['./test/**/*.js'], {read: false})
+    .pipe(mocha({reporter: 'nyan'}))
+    .on('error',gutil.log);
+});
+
+// run 'gulp watch' to watch all .js files and have livereload listen for changes
 gulp.task('watch', function(){
   livereload.listen();
   gulp.watch('./server/**/*.js');
