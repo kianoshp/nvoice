b54cc16a3f69bab569dc8a836c081e431c38e850
Ross <rdegnen@gmail.com>
2015-05-29 10:53:22 -0400

    added gulp-livereload

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,17 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var livereload = require('gulp-livereload');
 
-// run 'gulp jshint' in the console to lint every js file under the server folder
+// run 'gulp jshint' in the console to run jshint on every .js file under the server folder
 // and all its sub folders
 gulp.task("jshint", function(){
   gulp.src("./server/**/*.js")
     .pipe(jshint())
     .pipe(jshint.reporter("jshint-stylish"));
+});
+
+gulp.task('watch', function(){
+  livereload.listen();
+  gulp.watch('./server/**/*.js');
 });
