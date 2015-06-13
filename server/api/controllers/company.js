6fa28d910bef7b8143822fefc633528b7b5c22c5
kianoshp <kianoshp@gmail.com>
2015-06-12 21:48:14 -0400

    did some minor house cleaning and added a proper jshintrc file

--- server/api/controllers/company.js
+++ server/api/controllers/company.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var mongoose = require('mongoose');
 var Company = require('../models/company');
 var _ = require('lodash');
--- server/routes/company.js
+++ server/routes/company.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var companyAPI = require('./../api/controllers/company');
 
