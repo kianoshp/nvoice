13dab51abf8e9102c25603a2c7bf11f3724e7d37
Ross <rdegnen@gmail.com>
2015-06-15 10:28:33 -0400

    linted routes

--- server/routes/company.js
+++ server/routes/company.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var companyAPI = require('./../api/controllers/company');
 
-var company = function(app){
+var company = function(app) {
 
-  app.post('/company/create', function(req, res){
+  app.post('/company/create', function(req, res) {
     var thisCompany = companyAPI.createCompanyObj(req);
 
-    var newCompany = companyAPI.createCompany(thisCompany, function(err, companyObj){
+    var newCompany = companyAPI.createCompany(thisCompany, function(err, companyObj) {
       if (err) throw err;
 
       res.json(companyObj);
@@ -14,25 +14,25 @@ var company = function(app){
     });
   });
 
-  app.get('/company/read', function(req, res){
+  app.get('/company/read', function(req, res) {
     var companyId = req.query.companyId || req.body.companyId;
 
-    companyAPI.readCompany(companyId, function(err, company){
+    companyAPI.readCompany(companyId, function(err, company) {
       if (err) throw err;
       res.json(company);
     });
   });
 
-  app.put('/company/update', function(req, res){
+  app.put('/company/update', function(req, res) {
     companyAPI.updateCompany(req.body.companyId, req.body.companyObj, {new: true}, function(err, company) {
-      if(err) throw err;
+      if (err) throw err;
 
       res.json(company);
     });
   });
 
   app.delete('/company/delete', function(req, res) {
-    console.log("deleting company");
+    console.log('deleting company');
     companyAPI.deleteCompany(req.body.companyId);
       res.setHeader('Content-Type', 'application/json');
       res.send({status: 'complete', isRemoved: true});
