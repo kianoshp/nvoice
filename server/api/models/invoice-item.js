af3cc22e2b632a199729ae557cde5e22371dab4e
Ross <rdegnen@gmail.com>
2015-06-16 12:23:35 -0400

    created invoice-item model

---
+++ server/api/models/invoice-item.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var db = require('./db');
+
+var InvoiceItemSchema = new db.Schema({
+  invoiceId: db.Schema.ObjectId,
+  description: String,
+  qty: Number,
+  rate: Number,
+  isFlatFee: {
+      type: Boolean,
+      default: false
+  }
+});
+
+module.exports = mongoose.model('invoiceItem', InvoiceItemSchema);
