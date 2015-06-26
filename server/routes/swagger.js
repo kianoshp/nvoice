0acefff04c631fcc770e084215abcd40470cad78
Ross <rdegnen@gmail.com>
2015-06-26 13:55:16 -0400

    going to push up what I have so far

--- server/routes/swagger.js
+++ server/routes/swagger.js
@@ -4,7 +4,7 @@ var express = require('express');
 var path = require('path');
 
 var swagger = function(app) {
-  app.get('/server/api/docs/swagger.json', express.static(path.join(__dirname, '../swagger-ui/dist')));
+  app.get('/server/api/docs/swagger.json', express.static('../swagger-ui/dist'));
 };
 
 module.exports = swagger;
