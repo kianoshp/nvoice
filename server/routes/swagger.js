3843f25e1276e7a2c80140530f23a7bf3af48f2c
Ross <rdegnen@gmail.com>
2015-06-26 11:03:37 -0400

    properly cloned swagger-ui

--- server/routes/swagger.js
+++ server/routes/swagger.js
@@ -4,7 +4,7 @@ var express = require('express');
 var path = require('path');
 
 var swagger = function(app) {
-  app.get('/swagger', express.static(path.join(__dirname, '../swagger-ui')));
+  app.get('/server/api/docs/swagger.json', express.static(path.join(__dirname, '../swagger-ui/dist')));
 };
 
 module.exports = swagger;
